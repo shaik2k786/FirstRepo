cd7c7d2a72658ff75d62ec5c52bccd037e80f283
Saidashaik786 <108187239+Saidashaik786@users.noreply.github.com>
2023-06-09 13:46:20 +0530

    git practice

---
+++ src/com/demo/Test.java
@@ -0,0 +1,10 @@
+package com.demo;
+
+public class Test {
+public static void main(String[] args) {
+	int arr[] = {10,20,30,40,60,70,};
+	for(int i=0;i<=arr.length-1;i++) {
+		System.out.println(arr[i]);
+	}
+}
+}
